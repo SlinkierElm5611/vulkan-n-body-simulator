0d3a0039d5d9fff68386e72447ba58ff6be5d6a8
Stefan Balta <sbalta@Stefans-MacBook-Pro.local>
2025-03-31 20:23:30 -0400

    Added initial vulkan device creation, vma setup, and dynamic dispatch loader setup

--- main.cpp
+++ main.cpp
@@ -1,6 +1,112 @@
 #include <iostream>
+#define VMA_IMPLEMENTATION
+#define VMA_VULKAN_VERSION 1002000
+#include <vk_mem_alloc.h>
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+#include <vulkan/vulkan.hpp>
+#include <vector>
+
+VULKAN_HPP_DEFAULT_DISPATCH_LOADER_DYNAMIC_STORAGE
+
+class NBodySimulator {
+    private:
+        vk::Instance m_instance;
+        vk::PhysicalDevice m_physicalDevice;
+        vk::PhysicalDeviceType m_physicalDeviceType;
+        vk::Device m_device;
+        vk::Queue m_queue;
+        VmaAllocator m_allocator;
+        vk::CommandPool m_commandPool;
+        vk::CommandBuffer m_commandBuffer;
+        void createInstance() {
+            VULKAN_HPP_DEFAULT_DISPATCHER.init();
+            vk::ApplicationInfo appInfo{};
+            appInfo.pApplicationName = "NBodySimulator";
+            appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.pEngineName = "NBodySimulator";
+            appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+            appInfo.apiVersion = VK_API_VERSION_1_2;
+            std::vector<const char*> instanceExtensions = {
+                VK_KHR_SURFACE_EXTENSION_NAME,
+                VK_KHR_PORTABILITY_ENUMERATION_EXTENSION_NAME,
+            };
+            std::vector<const char*> instanceLayers = {
+                "VK_LAYER_KHRONOS_validation",
+            };
+            vk::InstanceCreateInfo createInfo{};
+            createInfo.pApplicationInfo = &appInfo;
+            createInfo.enabledExtensionCount = static_cast<uint32_t>(instanceExtensions.size());
+            createInfo.ppEnabledExtensionNames = instanceExtensions.data();
+            createInfo.enabledLayerCount = static_cast<uint32_t>(instanceLayers.size());
+            createInfo.ppEnabledLayerNames = instanceLayers.data();
+            createInfo.flags = vk::InstanceCreateFlagBits::eEnumeratePortabilityKHR;
+            m_instance = vk::createInstance(createInfo);
+            VULKAN_HPP_DEFAULT_DISPATCHER.init(m_instance);
+        }
+        void createPhysicalDevice() {
+            std::vector<vk::PhysicalDevice> physicalDevices = m_instance.enumeratePhysicalDevices();
+            std::vector<vk::PhysicalDeviceType> deviceTypes = {
+                vk::PhysicalDeviceType::eDiscreteGpu,
+                vk::PhysicalDeviceType::eIntegratedGpu,
+                vk::PhysicalDeviceType::eCpu,
+            };
+            for (const auto& deviceType : deviceTypes) {
+                for (const auto& physicalDevice : physicalDevices) {
+                    vk::PhysicalDeviceProperties properties = physicalDevice.getProperties();
+                    if (properties.deviceType == deviceType) {
+                        m_physicalDevice = physicalDevice;
+                        m_physicalDeviceType = deviceType;
+                        break;
+                    }
+                }
+                if (m_physicalDevice) {
+                    break;
+                }
+            }
+        }
+        void createDevice() {
+            std::vector<const char*> deviceExtensions = {
+                VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+                "VK_KHR_portability_subset",
+            };
+            std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos;
+            std::vector<float> queuePriorities = {1.0f};
+            vk::DeviceQueueCreateInfo queueCreateInfo{};
+            queueCreateInfo.queueFamilyIndex = 0;
+            queueCreateInfo.queueCount = 1;
+            queueCreateInfo.pQueuePriorities = queuePriorities.data();
+            queueCreateInfos.push_back(queueCreateInfo);
+            vk::DeviceCreateInfo createInfo{};
+            createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+            createInfo.pQueueCreateInfos = queueCreateInfos.data();
+            createInfo.enabledExtensionCount = static_cast<uint32_t>(deviceExtensions.size());
+            createInfo.ppEnabledExtensionNames = deviceExtensions.data();
+            m_device = m_physicalDevice.createDevice(createInfo);
+            m_queue = m_device.getQueue(0, 0);
+            VULKAN_HPP_DEFAULT_DISPATCHER.init(m_device);
+        }
+        void createAllocator() {
+            VmaAllocatorCreateInfo allocatorInfo{};
+            allocatorInfo.physicalDevice = m_physicalDevice;
+            allocatorInfo.device = m_device;
+            allocatorInfo.instance = m_instance;
+            vmaCreateAllocator(&allocatorInfo, &m_allocator);
+        }
+    public:
+        NBodySimulator() {
+            createInstance();
+            createPhysicalDevice();
+            createDevice();
+            createAllocator();
+        }
+        ~NBodySimulator() {
+            vmaDestroyAllocator(m_allocator);
+            m_device.destroy();
+            m_instance.destroy();
+        }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    NBodySimulator nBodySimulator;
     return 0;
 }
