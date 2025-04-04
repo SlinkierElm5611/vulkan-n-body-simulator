2031e09bee0c8ebcbd3d24ad4f963cac9f764a77
Stefan Balta <sbalta@Stefans-MacBook-Pro.local>
2025-04-04 12:21:28 -0400

    Added initial vertex and fragment shader implementation

--- vert.glsl
+++ vert.glsl
@@ -1,3 +1,8 @@
 #version 450
 
-void main() {}
+layout (location = 0) in vec2 inVertex;
+layout (location = 1) in vec2 inPosition;
+
+void main() {
+    gl_Position = vec4((inVertex + inPosition), 0.0, 1.0);
+    }
